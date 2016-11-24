672817069928f96255a586860e3c63733367fc97
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2016-11-24 19:53:49 +0100

    make it better

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ bot.dialog('/', (session) => {
 const entity = res.get('pokemon')
 if (intent) {
   INTENTS[intent.slug](entity)
-  .then(res => { res.forEach((message) => session.send(message)) })
-  .catch(err => { err.forEach((message) => session.send('Je ne comprends pas encore tout très bien, il faut être patient avec moi. Je suis un jeune bot qui doit apprendre.')) })
+  .then(res => session.send(res)) // If intent is infopokemon & no error: ‘OK’
+  .catch(err => session.send(err)) // If intent is infopokemon & error: ‘ERROR’
 }
  })
   .catch(() => session.send('Je ne comprends pas encore tout très bien, il faut être patient avec moi. Je suis un jeune bot qui doit apprendre.'))

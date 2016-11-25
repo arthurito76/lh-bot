dfe065b957618e6482c4b7978234fce5c30407d4
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-11-25 15:26:03 +0100

    make it better

--- intents/saladeAnswer.js
+++ intents/saladeAnswer.js
@@ -1,5 +1,4 @@
-const toText = message => ({ type: 'text', content: message } )
-const toImage = imageUrl => ({ type: 'image', content: imageUrl } )
+utils = require('./util.js')
 const saladeAnswer = () => {
   const answer = []
 answer.push(toImage('http://www.universal-connect.com/wp-content/themes/universal-connect/imgs/logo-universal-connect.png'))

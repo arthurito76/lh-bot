0c0957167a75d7d43ca345f6015b9d8b591639ee
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-11-25 17:04:00 +0100

    make it better

--- intents/greetings.js
+++ intents/greetings.js
@@ -1,15 +1,12 @@
 const utils = require('./util.js')
 const random = array => { return array[Math.floor(Math.random() * array.length)] }
 const getGreetings = () => {
-  const answer = [
-    'A va ti ?',
-    'Yo ;)',
-    'Hey, nice to see you.',
-    'Welcome back!',
-    'Hi, how can I help you?',
-    'Hey, what do you need?',
-  ]
-  return Promise.resolve([random(answer)])
+  const answer = []
+answer.push(utils.toText('Hello!'))
+ answer.push(utils.toText('Salut toi mon bézot'))	
+  answer.push(utils.toText('A va ti ?'))	
+    answer.push(utils.toText('yo'))	
+  return Promise.resolve([random(answers)])
   
 }
 module.exports = getGreetings

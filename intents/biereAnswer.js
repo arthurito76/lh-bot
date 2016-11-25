b799d36f7f7b653d2855150042a37b3cc480b5ce
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-11-25 16:31:25 +0100

    make it better

--- intents/biereAnswer.js
+++ intents/biereAnswer.js
@@ -1,7 +1,9 @@
 const utils = require('./util.js')
 const biereAnswer = () => {
   const answer = []
-answer.push(utils.toText('Pour acheter une bonne bière faut aller à la Cave à Bière. En plus tu pourras savourer une expo au passage','Côté bars il y a le Trappiste à Niemeyer qui a le plus grand choix', 'Et l\'Abri Côtier pour la bière et l\'équipe of course '))
+  answer.push(utils.toText('Pour acheter une bonne bière faut aller à la Cave à Bière. En plus tu pourras savourer une expo au passage'))
+    answer.push(utils.toText('Côté bars il y a le Trappiste à Niemeyer qui a le plus grand choix'))
+answer.push(utils.toText('Et l\'Abri Côtier pour la bière et l\'équipe of course '))
   return Promise.resolve(answer)
 }
 module.exports = biereAnswer

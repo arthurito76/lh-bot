0e6b488d3fd5d55e52e00b02b380a1b6fedaeaf2
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-11-25 18:09:44 +0100

    make it better

--- intents/saladeAnswer.js
+++ intents/saladeAnswer.js
@@ -1,7 +1,7 @@
 const utils = require('./util.js')
 const saladeAnswer = () => {
   const answer = []
-answer.push(utils.toImage('./img/profil.png'))
+answer.push(utils.toImage('http://www.aperorestodisco.com/DOCS/photos/5079/bar-restaurant-le-havre-l-eau-tarie-5079-2.jpeg.jpg'))
 answer.push(utils.toText('une bonne salade!'))
    return Promise.resolve(answer)
 }

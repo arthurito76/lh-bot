1a70fafbeded2da17f2675d064c231aa15c16968
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-11-25 12:58:59 +0100

    make it better

--- intents/saladeAnswer.js
+++ intents/saladeAnswer.js
@@ -1,10 +1,9 @@
 const toText = message => ({ type: 'text', content: message } )
 const toImage = imageUrl => ({ type: 'image', content: imageUrl } )
-const random = array => { return array[Math.floor(Math.random() * array.length)] }
 const saladeAnswer = () => {
   const answer = []
 answer.push(toImage('http://www.universal-connect.com/wp-content/themes/universal-connect/imgs/logo-universal-connect.png'))
 answer.push(toText(‘une bonne salade!’))
-   return Promise.resolve([random(answers)])
+   return Promise.resolve(answers)
 }
 module.exports = saladeAnswer

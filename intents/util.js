74952fcb97ab358b698cc6fe8f47c2ef1f1c7843
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-11-25 15:54:07 +0100

    make it better

--- intents/util.js
+++ intents/util.js
@@ -1,5 +1,5 @@
 const utils = {
-  toImage: imageUrl => ({ type: 'image', content: imageUrl } )
+  toImage: imageUrl => ({ type: 'image', content: imageUrl } ),
   toText: message => ({ type: 'text', content: message } )
 }
 module.exports = utils

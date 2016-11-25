112e3931e779645a3f8a1973fff8870e068f6c65
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-11-25 14:29:51 +0100

    make it better

--- intents/saladeAnswer.js
+++ intents/saladeAnswer.js
@@ -4,6 +4,7 @@ const saladeAnswer = () => {
   const answer = []
 answer.push(toImage('http://www.universal-connect.com/wp-content/themes/universal-connect/imgs/logo-universal-connect.png'))
 answer.push(toText('une bonne salade!'))
+console.log(answer);
    return Promise.resolve(answer)
 }
 module.exports = saladeAnswer

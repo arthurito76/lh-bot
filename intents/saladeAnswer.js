0b4e819f8412c69ce3006e48bd8fd1a2718b8696
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-11-25 14:03:56 +0100

    make it better

--- intents/saladeAnswer.js
+++ intents/saladeAnswer.js
@@ -3,7 +3,7 @@ const toImage = imageUrl => ({ type: 'image', content: imageUrl } )
 const saladeAnswer = () => {
   const answer = []
 answer.push(toImage('http://www.universal-connect.com/wp-content/themes/universal-connect/imgs/logo-universal-connect.png'))
-answer.push(toText(‘une bonne salade!’))
+answer.push(toText('une bonne salade!'))
    return Promise.resolve(answers)
 }
 module.exports = saladeAnswer

9de248251b5186dae83772d90fb9259607e01657
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-11-25 17:26:55 +0100

    make it better

--- intents/boitesAnswer.js
+++ intents/boitesAnswer.js
@@ -1,10 +1,11 @@
+const utils = require('./util.js')
 const random = array => { return array[Math.floor(Math.random() * array.length)] }
 const boitesAnswer = () => {
-  const answers = [
-    'Désolé, pour la vie nocturne il faut quitter le Havre',
-    'Mauvaise question, la mairie n\'aime pas la vie nocturne',
-    'Cela n\'est pas disponible au Havre',
-  ]
-   return Promise.resolve([random(answers)])
+  const answer = []
+answer.push(utils.toText('Désolé, pour la vie nocturne il faut quitter le Havre'))
+ answer.push(utils.toText('Salut toi mon bézot'))	
+  answer.push(utils.toText('Cela n\'est pas disponible au Havre'))	
+  return Promise.resolve([random(answer)])
+  
 }
 module.exports = boitesAnswer
--- intents/cavaAnswer.js
+++ intents/cavaAnswer.js
@@ -1,10 +1,11 @@
+const utils = require('./util.js')
 const random = array => { return array[Math.floor(Math.random() * array.length)] }
-const getGreetings = () => {
-  const answers = [
-    'Ca roule ça roule. 2 ou 3 boulons qui se rayent mais c\'est l\'air de la mer qui rouillent ma carcasse',
-    'Ca va ma foi, content de te voir',
-    'C\'est gentil de prendre de mes nouvelles !',
-  ]
-return Promise.resolve([random(answers)])
+const cavaAnswer = () => {
+  const answer = []
+answer.push(utils.toText('Ca roule ça roule. 2 ou 3 boulons qui se rayent mais c\'est l\'air de la mer qui rouillent ma carcasse'))
+ answer.push(utils.toText('Ca va ma foi, content de te voir'))	
+  answer.push(utils.toText('C\'est gentil de prendre de mes nouvelles !'))	
+  return Promise.resolve([random(answer)])
+  
 }
-module.exports = getGreetings
+module.exports = cavaAnswer

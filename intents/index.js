3ad3fa34e9498966499faf0cf12a6792174debb0
Arthur FORTIN <Arthur@MacBook-Pro-de-Guillaume.local>
2016-11-29 15:43:07 +0100

    make it better

--- intents/index.js
+++ intents/index.js
@@ -1,32 +1,32 @@
-module.exports : {    
- getInfoPokemon : require('./infopokemon.js')
- manger : require('./mangerAnswer.js')
- promenade : require('./promenadeAnswer.js')
-greetings : require('./greetings.js')
- merci : require('./merciAnswer.js')
- homeapero : require('./homeaperoAnswer.js')
- sortir : require('./sortirAnswer.js')
- vinAnswer : require('./vinAnswer.js')
- libanais : require('./libanaisAnswer.js')
-the : require('./theAnswer.js')
- hotel : require('./hotelAnswer.js')
- pizzalh : require('./pizzaAnswer.js')
- boites : require('./boitesAnswer.js')
-aurevoir : require('./byeAnswer.js')
- concert : require('./concertAnswer.js')
- gastro : require('./gastroAnswer.js')
-  cava : require('./cavaAnswer.js')
-  apero : require('./aperoAnswer.js')
- burger : require('./burgerAnswer.js')
- moules : require('./mouleAnswer.js')
- salade : require('./saladeAnswer.js')
-  frites : require('./friteAnswer.js')
- bouchons : require('./bouchonsAnswer.js')
- biere : require('./biereAnswer.js')
- sushis : require('./sushisAnswer.js')
- chinois : require('./chinoisAnswer.js')
- kebab : require('./kebabAnswer.js')
- insultes : require('./insultesAnswer.js')
- cocktails : require('./cocktailsAnswer.js')
- musee : require('./museeAnswer.js')    
- } 
+module.exports = {
+ getInfoPokemon : require('./infopokemon.js'),
+ manger: require('./mangerAnswer.js'),
+ promenade : require('./promenadeAnswer.js'),
+greetings : require('./greetings.js'),
+ merci : require('./merciAnswer.js'),
+ homeapero : require('./homeaperoAnswer.js'),
+ sortir : require('./sortirAnswer.js'),
+ vinAnswer : require('./vinAnswer.js'),
+ libanais : require('./libanaisAnswer.js'),
+the : require('./theAnswer.js'),
+ hotel : require('./hotelAnswer.js'),
+ pizzalh : require('./pizzaAnswer.js'),
+ boites : require('./boitesAnswer.js'),
+aurevoir : require('./byeAnswer.js'),
+ concert : require('./concertAnswer.js'),
+ gastro : require('./gastroAnswer.js'),
+  cava : require('./cavaAnswer.js'),
+  apero : require('./aperoAnswer.js'),
+ burger : require('./burgerAnswer.js'),
+ moules : require('./mouleAnswer.js'),
+ salade : require('./saladeAnswer.js'),
+  frites : require('./friteAnswer.js'),
+ bouchons : require('./bouchonsAnswer.js'),
+ biere : require('./biereAnswer.js'),
+ sushis : require('./sushisAnswer.js'),
+ chinois : require('./chinoisAnswer.js'),
+ kebab : require('./kebabAnswer.js'),
+ insultes : require('./insultesAnswer.js'),
+ cocktails : require('./cocktailsAnswer.js'),
+ musee : require('./museeAnswer.js')    ,
+}

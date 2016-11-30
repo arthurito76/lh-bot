f3fe7ba1d390cb78d5764b619e399553ec82c6fe
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-11-30 16:58:36 +0100

    make it better

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ bot.dialog('/', (session) => {
     const intent = res.intent()
 if (intent) {
 const restaurantName = res.get('restoinfo')	
-INTENTS[intent.slug](restoinfo)
+INTENTS[intent.slug](restaurantName)
 .then(res => { res.forEach((message) => sendMessageByType(session, message)) }) 
 .catch(err => { err.forEach((message) => sendMessageByType(session, message)) }) 
 } else { session.send(['Je ne comprends pas encore tout très bien, il faut être patient avec moi. Je suis un jeune bot qui doit apprendre.', 'ouhlala j\'ai encore beaucoup à apprendre tu sais', 'no comprendo senior', 'hein ?', 'je n\'ai pas la réponse mais je vais me renseigner']) }

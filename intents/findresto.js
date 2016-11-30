d6be4f4d4f53d09cc59045bb003e737652f7c973
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-11-30 16:27:38 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -1,6 +1,6 @@
 const _ = require('lodash') 
 const datas = require('./data.js') 
-const findresto = ('restoinfo') => { 
+const findresto = (restoinfo) => { 
 const object = lodash.filter(datas, data => data.name === entity.raw) 
 } 
 module.exports = findresto

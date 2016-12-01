f5cd4090af735a2e594874e4c6cd8e77ce90534c
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-01 12:18:34 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -1,9 +1,9 @@
 const _ = require('lodash') 
 const datas = require('./data.js') 
 const findresto = (RESTOINFO) => { 
+if (!RESTOINFO) { return Promise.reject('Quel resto ?')}
 console.log(RESTOINFO)
 const object = _.find (datas, data => data.name === RESTOINFO.raw) 
 console.log(object)
-return Promise.resolve(object)
 } 
 module.exports = findresto

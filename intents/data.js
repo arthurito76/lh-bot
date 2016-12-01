c534cd5e1d67cfa5a3e86c2b5f6a0e69658075d8
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2016-12-01 19:44:38 +0100

    make it better

--- intents/data.js
+++ intents/data.js
@@ -14,5 +14,40 @@ const datas = [
     location: 'Niemeyer',
     page: 'https://www.facebook.com/LeTrappistLeHavre/?fref=ts'
   },
+  {
+    name: 'O\'Brother',
+    location: 'Niemeyer',
+    page: 'https://www.facebook.com/obrotherlh'
+  },
+    {
+    name: 'L\'Etable',
+    location: 'rues Piétonnes',
+    page: 'https://www.facebook.com/etable76'
+  },
+    {
+    name: 'Mac Daids',
+    location: 'rues Piétonnes',
+    page: 'https://www.facebook.com/mc-daids-242943955526'
+  },
+     {
+    name: 'Mac Daids',
+    location: 'rues Piétonnes',
+    page: 'https://www.facebook.com/mc-daids-242943955526'
+  },
+       {
+    name: 'Les Zazous',
+    location: 'près de l\'Hôtel de Ville',
+    page: 'https://www.facebook.com/leszazouslehavre'
+  },
+         {
+    name: 'Wallaby Bar',
+    location: 'près de l\'Hôtel de Ville',
+    page: 'https://www.facebook.com/wallaby.bar.7'
+  },
+           {
+    name: 'Au Bureau',
+    location: 'quartier des Halles',
+    page: 'https://www.facebook.com/aubureau.lehavre'
+  },
 ]
 module.exports = datas

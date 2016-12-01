891143a0c14966752e9dd2476b5b638e65bc3e61
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-01 10:41:14 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -4,5 +4,6 @@ const findresto = (RESTOINFO) => {
 console.log(RESTOINFO)
 const object = _.find (datas, data => data.name === RESTOINFO.raw) 
 console.log(object)
+return Promise.resolve([random(object)])
 } 
 module.exports = findresto

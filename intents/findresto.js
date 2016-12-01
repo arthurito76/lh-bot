6555000e0ba9a291e4201b0e07e1f439e2d0d80c
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-01 14:07:32 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -5,7 +5,7 @@ const findresto = (RESTOINFO) => {
  if (!RESTOINFO) { return Promise.resolve([utils.toText('De quel restaurant parlez vous?')])}
 else  { 
 console.log(RESTOINFO)
-const object = _.find (datas, data => data.name === RESTOINFO.raw) 
+const object =_.find(datas, data => data.name === RESTOINFO.raw) 
 console.log(object)
 } } 
 module.exports = findresto

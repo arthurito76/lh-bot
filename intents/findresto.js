9ab36b50f1db9f2405df25763c986b2fecd9fbc0
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-01 14:33:42 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -3,9 +3,10 @@ const utils = require('./util.js')
 const datas = require('./data.js') 
 const findresto = (RESTOINFO) => { 
  if (!RESTOINFO) { return Promise.resolve([utils.toText('De quel restaurant parlez vous?')])}
-else  { 
 console.log(RESTOINFO)
 const object =_.find(datas, data => data.name === RESTOINFO.raw) 
 console.log(object)
-} } 
+if (!object) { return Promise.resolve([utils.toText('Je ne trouve pas ce resto')])}
+return Promise.resolve([utils.toText('Je connais ce restaurant il est situé ' + object.location)])
+} 
 module.exports = findresto

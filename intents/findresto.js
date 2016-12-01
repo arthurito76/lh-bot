3682097c561b5712f1e4906a3a2caa04245e3a67
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-01 10:02:32 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -1,6 +1,7 @@
 const _ = require('lodash') 
 const datas = require('./data.js') 
 const findresto = (RESTOINFO) => { 
+console.log(RESTOINFO)
 const object = _.find (datas, data => data.name === RESTOINFO.raw) 
 console.log(object)
 } 

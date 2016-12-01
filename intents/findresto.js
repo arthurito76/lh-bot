188e821fae5a7c759d56a8b3dd790302cc0e3a0a
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-01 17:22:20 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -5,10 +5,7 @@ const datas = require('./data.js')
 const findresto = (RESTOINFO) => { 
 console.log(datas) 
  if (!RESTOINFO) { return Promise.resolve([utils.toText('De quel restaurant parlez vous?')])}
-console.log(fuzzy)
-const distance = fuzzy.metrics.jaro_winkler(data.name, RESTOINFO.raw)
-console.log(distance)
-const object =_.find(datas, data => distance > 0.8)
+const object =_.find(datas, data => fuzzy.metrics.jaro_winkler(data.name, RESTOINFO.raw) > 0.8)
 console.log(object)
 if (!object) { return Promise.resolve([utils.toText('Je ne trouve pas ce resto')])}
 const answer = []

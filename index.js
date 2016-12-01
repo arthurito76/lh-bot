950c93638765a1b767fa469207722a4ad96f1ec3
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-01 16:52:39 +0100

    make it better

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ bot.dialog('/', (session) => {
     const intent = res.intent()
 if (intent) {
 const restaurantName = res.get('restoinfo')	
-console.log(restaurantName)
 INTENTS[intent.slug](restaurantName)
 .then(res => { res.forEach((message) => sendMessageByType(session, message)) }) 
 .catch(err => { err.forEach((message) => sendMessageByType(session, message)) }) 

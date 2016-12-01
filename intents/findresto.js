e0c135ee59d4c626c9a224de763833388baaf4b1
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-01 12:44:50 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -1,9 +1,10 @@
 const _ = require('lodash') 
 const datas = require('./data.js') 
 const findresto = (RESTOINFO) => { 
+ if (!entity) { return Promise.resolve([u.toText('De quel restaurant parlez vous?')])
+else  { 
 console.log(RESTOINFO)
 const object = _.find (datas, data => data.name === RESTOINFO.raw) 
 console.log(object)
-return Promise.resolve([u.toText('De quel restaurant parlez vous?')])
-} 
+} } 
 module.exports = findresto

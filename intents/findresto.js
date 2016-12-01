c8cc45abfeb0711cef3e5dde359369198764eb5c
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-01 16:28:07 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -6,7 +6,9 @@ const findresto = (RESTOINFO) => {
 console.log(datas) 
  if (!RESTOINFO) { return Promise.resolve([utils.toText('De quel restaurant parlez vous?')])}
 console.log(RESTOINFO)
-const object =_.find(datas, data => clj_fuzzy.metrics.jaro_winkler(data.name, RESTOINFO.raw) > 0.8)
+const distance = clj_fuzzy.metrics.jaro_winkler(data.name, RESTOINFO.raw)
+console.log(distance)
+const object =_.find(datas, data => distance > 0.8)
 console.log(object)
 if (!object) { return Promise.resolve([utils.toText('Je ne trouve pas ce resto')])}
 const answer = []

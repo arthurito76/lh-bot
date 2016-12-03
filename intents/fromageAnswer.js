738b604b4a4a0d716a44f24cada1fe352eec0fab
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2016-12-03 16:33:44 +0100

    make it better

--- intents/fromageAnswer.js
+++ intents/fromageAnswer.js
@@ -1,9 +1,9 @@
 const random = array => { return array[Math.floor(Math.random() * array.length)] }
 const fromageAnswer = () => {
   const answer = []
-  answer.push(toText('Les meilleurs fromagers sont aux Halles'))
+  answer.push(utils.toText('Les meilleurs fromagers sont aux Halles'))
 answer.push(utils.toImage('http://arthurfortin.fr/images/from.jpeg'))
-  answer.push(toText('Niveau resto je te conseille la Petite Savoie'))
+  answer.push(utils.toText('Niveau resto je te conseille la Petite Savoie'))
 answer.push(utils.toImage('http://arthurfortin.fr/images/savoie.jpg'))
    return Promise.resolve([random(answers)])
 }

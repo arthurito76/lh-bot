c54ded7c45ec7ef99bb8e0c2ec1f9a722707a328
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2016-12-03 16:40:46 +0100

    make it better

--- intents/bioAnswer.js
+++ intents/bioAnswer.js
@@ -1,12 +1,13 @@
-const random = array => { return array[Math.floor(Math.random() * array.length)] }
+const utils = require('./util.js')
 const bioAnswer = () => {
   const answer = []
   answer.push(utils.toText('Alors la plus grande boutique bio à LH est Naturéo au Grand Hameau'))
 answer.push(utils.toImage('http://arthurfortin.fr/images/natur.jpeg'))
-  answer.push(utils.toText('Rayons Verts avenue Coty’))
+  answer.push(utils.toText('Rayons Verts avenue Coty'))
 answer.push(utils.toImage('http://arthurfortin.fr/images/ray.jpeg'))
- answer.push(utils.toText('Le marché Bio rue de Paris’))
+ answer.push(utils.toText('Le marché Bio rue de Paris'))
 answer.push(utils.toImage('http://arthurfortin.fr/images/natur.jpeg'))
-   return Promise.resolve([random(answers)])
+  
+   return Promise.resolve(answer)
 }
 module.exports = bioAnswer
--- intents/fromageAnswer.js
+++ intents/fromageAnswer.js
@@ -1,10 +1,11 @@
-const random = array => { return array[Math.floor(Math.random() * array.length)] }
+const utils = require('./util.js')
 const fromageAnswer = () => {
   const answer = []
   answer.push(utils.toText('Les meilleurs fromagers sont aux Halles'))
 answer.push(utils.toImage('http://arthurfortin.fr/images/from.jpeg'))
   answer.push(utils.toText('Niveau resto je te conseille la Petite Savoie'))
 answer.push(utils.toImage('http://arthurfortin.fr/images/savoie.jpg'))
-   return Promise.resolve([random(answers)])
+  
+   return Promise.resolve(answer)
 }
 module.exports = fromageAnswer

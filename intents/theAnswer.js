3c8a761d8e1ff8de7e802152b2bc74cc49e88f14
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2016-12-04 09:20:11 +0100

    make it better

--- intents/theAnswer.js
+++ intents/theAnswer.js
@@ -2,7 +2,6 @@ const utils = require('./util.js')
 const theAnswer = () => {
   const answer = []
 answer.push(utils.toText('Sans hésiter je te réponds le Chat Bleu'))
-
 answer.push(utils.toText('Du bon thé, de bonnes choses à grignoter, une ambiance cool et un patron sympa'))
 answer.push(utils.toImage('http://arthurfortin.fr/images/chat.jpg'))
 answer.push(utils.toText('Et le 2ème meilleur de la ville c\'est l\'Air du Thé'))
--- intents/visiterAnswer.js
+++ intents/visiterAnswer.js
@@ -2,7 +2,7 @@ const utils = require('./util.js')
 const cineAnswer = () => {
   const answer = []
   answer.push(utils.toText('Ce n\'est pas compliqué : laisse Bee te faire découvrir la ville'))
-  answer.push(utils.toText('http://arthurfortin.fr/images/bee'))
+answer.push(utils.toImage('http://arthurfortin.fr/images/bee'))
     answer.push(utils.toText('RDV rues piétonnes'))
   return Promise.resolve(answer)
 }

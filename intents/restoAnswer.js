28f7bd64c0aa1c7da78cff70a6019cb31623476a
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-06 17:35:10 +0100

    make it better

--- intents/restoAnswer.js
+++ intents/restoAnswer.js
@@ -1,12 +1,12 @@
 const utils = require('./util.js')
-const restoAnswer = (RestaurantType) => {
-	if (RestaurantType == 'Indien') { 
+const restoAnswer = (RESTAURANTTYPE) => {
+	if (RESTAURANTTYPE == 'Indien') { 
 const answer = [] 
 answer.push(utils.toText('un bon resto indien')) 
 console.log(answer)
 return Promise.resolve(answer) }
 
-if (RestaurantType == 'Turc') { 
+if (RESTAURANTTYPE == 'Turc') { 
 const answer = [] 
 answer.push(utils.toText('un bon resto turc')) 
 return Promise.resolve(answer) }

66648d6925c53c4975802c79cda9a76fe907b733
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-07 14:11:08 +0100

    make it better

--- intents/activiteAnswer.js
+++ intents/activiteAnswer.js
@@ -7,6 +7,19 @@ const answer = []
 answer.push(utils.toText('la mer, LOL')) 
 return Promise.resolve(answer) }
 
+if (ACTIVITE.raw == 'promener') { 
+const answer = [] 
+  answer.push(utils.toText('La plus zen des promenades c\est aux Jardins Suspendus. Un vrai coin d\'exotisme au coeur de la ville'))
+  answer.push(utils.toImage('http://www.lehavre.fr/sites/all/files/imagecache/645x320/articles/_mea0063_0.jpg'))
+   answer.push(utils.toText('Il y a aussi la forêt de Montgeon, une forêt en pleine ville c\'est bath non ?'))
+answer.push(utils.toImage('http://www.lehavre.fr/sites/all/files/imagecache/645x320/articles/bando-foret-de-montgeon-645.jpg'))
+return Promise.resolve(answer) }
+
+if (ACTIVITE.raw == 'sortir') { 
+const answer = [] 
+answer.push(utils.toText('Tout dépend de ce que tu veux boire'))
+}
+return Promise.resolve(answer) }
 	
 if (ACTIVITE.raw == 'théâtre') { 
 const answer = [] 

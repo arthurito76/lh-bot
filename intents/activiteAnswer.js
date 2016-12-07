f4bde9d59ba20a1b535adc2c0545d4989eae4372
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-07 13:26:19 +0100

    make it better

--- intents/activiteAnswer.js
+++ intents/activiteAnswer.js
@@ -7,6 +7,20 @@ const answer = []
 answer.push(utils.toText('la mer, LOL')) 
 return Promise.resolve(answer) }
 
+if (ACTIVITE.raw == 'sport') { 
+const answer = [] 
+answer.push(utils.toText('Sage question. Alors je te conseillerais la piscine des Docks. Grand bassin, balnéo et salle de sport. Le lieu est beau et bien pourvu. '))
+answer.push(utils.toImage('http://arthurfortin.fr/images/pisdock.jpeg'))
+return Promise.resolve(answer) }
+
+if (ACTIVITE.raw == 'SPA') { 
+const answer = [] 
+answer.push(utils.toText('Alors l\'hôtel 4 étoiles Vent d\'Ouest en propose un haut de gamme'))
+answer.push(utils.toImage('http://arthurfortin.fr/images/vent.jpeg'))
+answer.push(utils.toText('Et la piscine des Docks en propose en plus d\'autres douceurs comme la balnéo et le hammam'))
+answer.push(utils.toImage('http://arthurfortin.fr/images/pisdock.jpeg'))
+return Promise.resolve(answer) }
+
 	if (ACTIVITE.raw == 'boîtes de nuit') { 
 const answer = [] 
 answer.push(utils.toText('Désolé, pour la vie nocturne il faut quitter le Havre. Je pourrais te dire d\'aller aux magasins généraux mais si tu veux rentrer vivant évite. La mairie ne veut pas de vie nocturne'))	
@@ -25,6 +39,8 @@ const answer = []
     answer.push(utils.toText('Ou le Sirius, plus en mode Télérama.'))
 	 answer.push(utils.toText('http://www.cinemasirius.com/cette-semaine'))
 return Promise.resolve(answer) }
+
+
 
 else { 
 const answer = [] 

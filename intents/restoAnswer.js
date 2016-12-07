a353d598e75e5619a84afde9c60083cb5a5531fe
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-07 10:59:44 +0100

    make it better

--- intents/restoAnswer.js
+++ intents/restoAnswer.js
@@ -29,6 +29,34 @@ answer.push(utils.toText('Alors côté épicerie tu as l\'italien qui se trouve 
   answer.push(utils.toImage('http://arthurfortin.fr/images/dente.jpg'))	
 return Promise.resolve(answer) }
 
+if (RESTAURANTTYPE.raw == 'libanais') { 
+const answer = [] 
+ answer.push(utils.toText('@O délices du Liban sans hésiter.'))
+  answer.push(utils.toText('Une cuisinière au top et des mets succulents'))
+  return Promise.resolve(answer) }
+  
+  if (RESTAURANTTYPE.raw == 'moules') { 
+const answer = [] 
+ answer.push(utils.toText('Va à la Taverne Paillette, c\'est l\'une des rares institutions que les anglais n\'aient pas rasé en 44'))
+answer.push(utils.toImage('http://arthurfortin.fr/images/frites.JPG'))
+ answer.push(utils.toText('Après si c\est l\'été, Saison 2 à la plage t\'en propose des biens fraîches'))
+  return Promise.resolve(answer) }
+  
+    if (RESTAURANTTYPE.raw == 'burger') { 
+const answer = [] 
+ answer.push(utils.toText('Tout d\'abord je dirais La Petite Rade, avec un des meilleurs spots de la ville en prime'))
+answer.push(utils.toImage('http://arthurfortin.fr/images/burgerrade.jpg'))
+ answer.push(utils.toText('Le Fifty\'s n\'est pas piqué des hanetons non plus'))
+ answer.push(utils.toImage('http://arthurfortin.fr/images/burgerfif.jpg'))
+    answer.push(utils.toText('Et n\'oublions pas de mentionner les Pénates non plus qui ne donne pas sa part aux lions'))
+  return Promise.resolve(answer) }
+  
+      if (RESTAURANTTYPE.raw == 'salade') { 
+const answer = [] 
+ answer.push(utils.toImage('http://www.aperorestodisco.com/DOCS/photos/5079/bar-restaurant-le-havre-l-eau-tarie-5079-2.jpeg.jpg'))
+answer.push(utils.toText('une bonne salade!'))
+  return Promise.resolve(answer) }
+
 if (RESTAURANTTYPE.raw == 'pizza') { 
 const answer = [] 
 answer.push(utils.toText('Tu peux aller à Al Dente'))

0f7ec0294d3ac454f2426a5dd796c9f2571c7ee7
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-07 18:09:15 +0100

    make it better

--- intents/restoAnswer.js
+++ intents/restoAnswer.js
@@ -8,6 +8,12 @@ answer.push(utils.toText('un bon resto indien'))
 console.log(answer)
 return Promise.resolve(answer) }
 
+	if (RESTAURANTTYPE.raw == 'crêpes') { 
+const answer = [] 
+answer.push(utils.toText('Les meilleures crêpes de la ville sont à Saint-François'))
+ answer.push(utils.toText('C\'est l\'ancien quartier breton donc autant te dire qu\'on ne rigole pas avec les crêpes chez eux '))	
+return Promise.resolve(answer) }
+
 if (RESTAURANTTYPE.raw == 'steak') { 
 const answer = [] 
 answer.push(utils.toText('Le Grignot te propose de belles pièces de viande'))

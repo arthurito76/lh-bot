03f8ab0f202430c3b46ec28c3c48b0718e29eb69
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-07 10:16:28 +0100

    make it better

--- intents/restoAnswer.js
+++ intents/restoAnswer.js
@@ -8,6 +8,13 @@ answer.push(utils.toText('un bon resto indien'))
 console.log(answer)
 return Promise.resolve(answer) }
 
+if (RESTAURANTTYPE.raw == 'chinois') { 
+const answer = [] 
+answer.push(utils.toText('Sans hésiter va à Saïgon à Saint-Vincent. C\'est à emporter et à savourer ;-)'))
+ answer.push(utils.toText('Le Palais Impérial si tu veux te poser'))	
+answer.push(utils.toImage('http://arthurfortin.fr/images/palais.jpg'))
+return Promise.resolve(answer) }
+
 if (RESTAURANTTYPE.raw == 'Turc') { 
 const answer = [] 
 answer.push(utils.toText('un bon resto turc')) 

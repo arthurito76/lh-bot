88ac189addf2cc5ca392a89dffaa38781149a500
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-08 09:14:21 +0100

    make it better

--- intents/activiteAnswer.js
+++ intents/activiteAnswer.js
@@ -7,6 +7,13 @@ const answer = []
 answer.push(utils.toText('la mer, LOL')) 
 return Promise.resolve(answer) }
 
+	if (ACTIVITE.raw == 'apéro') { 
+const answer = [] 
+answer.push(utils.toText('Voilà une vrai question... Tout dépend de ce que tu aimes'))
+answer.push(utils.toText('Perso j\'aime bien l\'Eautarie ou l\'Abricot avec les copains en mode bière et planche à grignoter'))
+answer.push(utils.toText('Si tu veux du plus guindé tu as l\Etable ou les Enfants Sages'))	
+return Promise.resolve(answer) }
+
 if (ACTIVITE.raw == 'piscine') { 
 const answer = [] 
 answer.push(utils.toText('Alors je te conseillerais la piscine des Docks. Grand bassin, balnéo et salle de sport. Le lieu est beau et bien pourvu. '))
---
+++ intents/commoditeAnswer.js
@@ -0,0 +1,16 @@
+const utils = require('./util.js')
+const commoditeAnswer = (RESTOINFO, RESTAURANTTYPE, ACTIVITE, COMMODITE) => {
+		if (!COMMODITE) { return Promise.resolve([utils.toText('Quelle est ta démarche ?')])}
+	
+	if (ACTIVITE.raw == 'mairie') { 
+const answer = [] 
+answer.push(utils.toText('ouhlalalala')) 
+return Promise.resolve(answer) }
+
+
+else { 
+const answer = [] 
+answer.push(utils.toText('Je vais me renseigner')) 
+return Promise.resolve(answer) }
+}
+module.exports = commoditeAnswer

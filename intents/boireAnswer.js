f60077075c4d7e67b54ba7cbf691834ee4855c5e
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-08 15:08:47 +0100

    make it better

--- intents/boireAnswer.js
+++ intents/boireAnswer.js
@@ -1,18 +1,12 @@
 const utils = require('./util.js')
 const boireAnswer = (RESTOINFO, RESTAURANTTYPE, ACTIVITE, COMMODITE, BOIRE) => {
-		if (!BOIRE) { return Promise.resolve([utils.toText('Quelle est ta démarche ?')])}
+		if (!BOIRE) { return Promise.resolve([utils.toText('Que veux-tu boire ?')])}
 	
-	if (BOIRE.raw == 'mairie') { 
+	if (BOIRE.raw == 'chocolat') { 
 const answer = [] 
 answer.push(utils.toText('ouhlalalala')) 
 return Promise.resolve(answer) }
 
-if (BOIRE.raw == 'médias') { 
-const answer = [] 
-answer.push(utils.toText('Alors on a une presse locale bien moribonde, et qui ne dit rien. '))
-    answer.push(utils.toText('près je te conseille Les Havraisemblables. Un blog indépendant bien écrit avec des choses vraiment intéressantes'))
-answer.push(utils.toText('http://leshavraisemblables.fr/'))
-return Promise.resolve(answer) }
 
 else { 
 const answer = [] 

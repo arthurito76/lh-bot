19afa08585d180cbdbfe533535cd6220a9d13eae
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2016-12-09 08:22:57 +0100

    make it better

--- intents/data.js
+++ intents/data.js
@@ -3,29 +3,34 @@ const datas = [
     name: 'l\'Eau Tarie',
     location: 'Rues piétonnes',
     page: 'https://www.facebook.com/www.leautarielehavre.fr/?fref=ts',
+	tags: ['bar', 'bière', 'vin', 'tapas'],
 	avis: 'Un bar bien sympa pour prendre un apéro avec une terrasse chauffée pour ne pas trop se les meuler.'
   },
   {
     name: 'l\'Abri Côtier',
     location: 'Plage',
     page: 'https://www.facebook.com/abricotierbar/?fref=ts',
+	tags: ['bar', 'bière', 'vin', 'tapas'],
 	avis: 'Pour moi le meilleur bar du Havre. Un emplacement parfait, une équipe au top et en plus on peut danser jusqu\'à 2h le samedi soir.'
   },
   {
     name: 'Le Trappiste',
     location: 'Niemeyer',
     page: 'https://www.facebook.com/LeTrappistLeHavre/?fref=ts',
+	tags: ['bar', 'bière', 'belge', 'tapas'],
 	avis: 'Un bar bien décoré pour celles et ceux qui aiment la bière qui fait mal au crâne.'
   },
   {
     name: 'O\'Brother',
     location: 'Niemeyer',
     page: 'https://www.facebook.com/obrotherlh',
+	tags: ['pub', 'bière', 'match', 'tapas', 'sport'],
 	avis: 'Un bar à l\'ambiance irish idéal pour se mater un match. A noter une bouffe excellente le midi.'
   },
     {
     name: 'L\'Etable',
     location: 'rues Piétonnes',
+	tags: ['bar', 'bière', 'vin', 'tapas'],
     page: 'https://www.facebook.com/etable76',
 	avis: 'Un lieu agréable et un peu cher.'
   },

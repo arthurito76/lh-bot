442ce0764846a07f2edb52b5c93ae24161cb3768
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2016-12-10 13:54:40 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -7,17 +7,12 @@ console.log(datas)
  if (!RESTOINFO) { return Promise.resolve([utils.toText('De quel lieux parles-tu ?')])}
 const object =_.find(datas, data => fuzzy.metrics.jaro_winkler(data.name, RESTOINFO.raw) > 0.8)
 console.log(object)
-if (!object) { return Promise.resolve([utils.toText('Je ne trouve pas ce resto')])}
+if (!object) { return Promise.resolve([utils.toText('Je ne trouve pas ce resto mais je vais me renseigner')])}
 const answer = []
   answer.push(utils.toText('localisation ' + object.location))
   answer.push(utils.toText('sa page facebook: ' + object.page))
   answer.push(utils.toText('avis : ' + object.avis))
    return Promise.resolve(answer)
-   
-   else { 
-const answer = [] 
-answer.push(utils.toText('Je n\'ai pas mais je vais tester ça au plus vite alors')) 
-return Promise.resolve(answer) }
 
 } 
 module.exports = findresto

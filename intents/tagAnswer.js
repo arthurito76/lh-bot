eea61d8a4e31d35ef6b051feda12baac3b63e47d
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2016-12-13 21:30:37 +0100

    make it better

--- intents/tagAnswer.js
+++ intents/tagAnswer.js
@@ -2,6 +2,8 @@ const utils = require('./util.js')
 const _ = require('lodash')
 const datas = require('./data.js') 
 const tagAnswer = (RESTOINFO, RESTAURANTTYPE, ACTIVITE, COMMODITE, ACHAT, TAGINFO) => {
+	
+	console.log (tagAnswer)
 		if (!TAGINFO) { return Promise.resolve([utils.toText('Précise ta recherche')])}
 
 const goodPlaces = _.filter(datas, place => place.tags.indexOf(TAGINFO.raw) !== -1)

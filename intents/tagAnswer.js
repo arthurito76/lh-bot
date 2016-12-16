4fa53ad770c2a4a9fa09056d7c6137ba906b929c
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-16 14:17:03 +0100

    make it better

--- intents/tagAnswer.js
+++ intents/tagAnswer.js
@@ -18,8 +18,8 @@ const answer = []
 answer.push(utils.toText('Yes, j\'ai trouvé quelque chose pour toi : ') ) 
 answer.push(utils.toText(goodPlaces[i].name + ' situé à ' + goodPlaces[i].location) ) 
 answer.push(utils.toText('mon avis : ' + goodPlaces[i].avis) ) 
-return Promise.resolve(answer) 
 }
+return Promise.resolve(answer) 
  
 }
 

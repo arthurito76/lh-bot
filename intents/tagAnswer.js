d56718d93992996fe293314b06f717476a966d5f
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-16 13:23:07 +0100

    make it better

--- intents/tagAnswer.js
+++ intents/tagAnswer.js
@@ -2,8 +2,7 @@ const utils = require('./util.js')
 const _ = require('lodash')
 const datas = require('./data.js') 
 const tagAnswer = (RESTOINFO, TAGINFO) => {
-	
-	console.log(datas) 
+	 
 		if (!TAGINFO) { return Promise.resolve([utils.toText('Précise ta recherche')])}
 
 const goodPlaces = _.filter(datas, place => place.tags.indexOf(TAGINFO.raw) !== -1)
@@ -13,7 +12,6 @@ if (goodPlaces.length === 0) {
 }
 
 if (goodPlaces.length > 1) {
-  return Promise.resolve('Oups, rien trouvé :-(')
 
  for (var i = 0; i <  goodPlaces.length;  i++) { 
 
@@ -22,6 +20,7 @@ answer.push(utils.toText('Yes, j\'ai trouvé quelque chose pour toi : ') )
 answer.push(utils.toText(goodPlaces[i].name + ' situé à ' + goodPlaces[i].location) ) 
 answer.push(utils.toText('mon avis : ' + goodPlaces[i].avis) ) 
 return Promise.resolve(answer) 
+console.log(answer)
 }}
  
 }

acb2ae9fc8fb8de3e7bdfc57ab2ef6dc7debff13
Arthur FORTIN <Arthur@uc-pc-tech-pv1.universal.local>
2016-12-16 12:59:40 +0100

    make it better

--- intents/tagAnswer.js
+++ intents/tagAnswer.js
@@ -11,7 +11,10 @@ const goodPlaces = _.filter(datas, place => place.tags.indexOf(TAGINFO.raw) !== 
 if (goodPlaces.length === 0) {
   return Promise.resolve('Oups, rien trouvé :-(')
 }
- for (var i = 0; len = goodPlaces.length; i < len; i++) { 
+
+
+
+ for (var i = 0; i <  goodPlaces.length;  i++) { 
 
 const answer = [] 
 answer.push(utils.toText('Yes, j\'ai trouvé quelque chose pour toi : ') ) 

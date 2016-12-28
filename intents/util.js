308d5d70faf54179d8626a65590b491e720e1e0a
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2016-12-28 20:38:48 +0100

    make it better

--- intents/util.js
+++ intents/util.js
@@ -1,5 +1,12 @@
 const utils = {
   toImage: imageUrl => ({ type: 'image', content: imageUrl } ),
   toText: message => ({ type: 'text', content: message } )
+  // Buttons
+ const toButtons = (title, buttons) => {
+ return { type: 'buttons', content: buttons, title }
+ }
+
+// Button
+ const toButton = (title, value) => { return { title, value } }
 }
 module.exports = utils

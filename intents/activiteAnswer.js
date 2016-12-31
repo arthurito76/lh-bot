6cd802ef9940a95da74c954cf79dd2c44a8e291d
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2016-12-31 13:53:07 +0100

    make it better

--- intents/activiteAnswer.js
+++ intents/activiteAnswer.js
@@ -1,14 +1,38 @@
 const utils = require('./util.js')
 const _ = require('lodash')
 const datas = require('./data.js') 
+const Fuzzy = require('fuzzy-matching')
+const fuzzyLocation = new Fuzzy(datas.reduce((prev, current) => {
+return [...prev, ...current.locationTag];
+}, []));
+
+const fuzzySpecialities = new Fuzzy(datas.reduce((prev, current) => {
+ return [...prev, ...current.tags];
+}, []));
 const random = array => { return array[Math.floor(Math.random() * array.length)] }
-const activiteAnswer = (RESTOINFO, TAGINFO, ACTIVITEINFO) => {
+const activiteAnswer = (RESTOINFO, SPECIALITIES, CUSTOMLOCATION, ACTIVITEINFO, ACHATINFO) => {
 	 
 		if (!ACTIVITEINFO.length) { return Promise.resolve([utils.toText('Que veux-tu faire exactement ?')])}
 
-const goodPlace = _.filter(datas, place => ACTIVITEINFO.every(tag => place.tags.indexOf(tag.raw) !== -1))
+var goodActivite = []
+ACTIVITEINFO.forEach(tag => {
+     const match = fuzzyAchatinfo.get(tag.raw);
+     if (match.distance > 0.8) {
+       goodActivite = _.filter(datas, place => place.tags.indexOf(match.value) !== -1)
+     }
+ })
+ 
+ if (goodActivite.length && CUSTOMLOCATION.length) {
+    CUSTOMLOCATION.forEach(tag => {
+       const match = fuzzyLocation.get(tag.raw);
+       if (match.distance > 0.8) {
+         goodActivite = _.filter(goodActivite, place => place.locationTag.indexOf(match.value) !== -1)
+       }
+   })
+}
 
-if (goodPlace.length === 0) {
+
+if (goodAchat.length === 0) {
    const answer = []
 answer.push(utils.toText('Je ne sais pas où te diriger mon bézot, je suis désolé'))
  answer.push(utils.toText('Là je n\'ai rien. Peut-être le moment de partir du Havre pour découvrir plus de choses à faire ?'))	
@@ -18,14 +42,22 @@ answer.push(utils.toText('Je ne sais pas où te diriger mon bézot, je suis dés
   }
 
 
-const answer = [] 
+ const answer = [] 
 answer.push(utils.toText('Yes, j\'ai trouvé quelque chose pour toi : ') )
- for (var i = 0, len = goodPlace.length; i < len; i++) {  
-answer.push(utils.toText(goodPlace[i].name + ' situé à ' + goodPlace[i].location) ) 
-answer.push(utils.toText('mon avis : ' + goodPlace[i].avis) ) 
+const cards = []
+for (var i = 0, len = goodActivite.length; i < len; i++) {  
+  const title = goodActivite[i].name + ' situé à ' + goodActivite[i].location
+  const image = goodActivite[i].image
+  const buttons = [
+    utils.toButton('Lire mon avis', 'lire mon avis sur ' + goodActivite[i].name, 'imBack'), //bouton 1
+	utils.toButton('page facebook',goodActivite[i].page , 'openUrl'), //bouton 2
+  ]
+
+  cards.push({ title, image, buttons })
 }
+answer.push(utils.toCarousel(cards))
+
 return Promise.resolve(answer) 
- 
 }
 
 module.exports = activiteAnswer

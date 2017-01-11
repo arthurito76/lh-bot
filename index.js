31a0ab51661f50e4b45c75a2baffe10a2a0dfa68
Arthur FORTIN <Arthur@uc-pc-tech-tp.universal.local>
2017-01-11 10:33:51 +0100

    make it better

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const bot = new builder.UniversalBot(connector)
 bot.dialog('/', (session) => {
 	 
   const user = session.userData
-
+console.log (user)
   recastClient.textRequest(session.message.text)
   .then(res => {
     const intent = res.intent()

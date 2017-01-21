f5a362a93b7e0514cd8c104e303be4518aa75bac
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2017-01-21 08:49:10 +0100

    make it better

--- getEntities.js
+++ getEntities.js
@@ -18,23 +18,20 @@ const getEntities = (res, user) => {
     user.locationType = entities.locationType
   } else if (user.locationType) {
     entities.locationType = user.locationType
-console.log('-->')
-console.log(user.locationType)
-	
-  }
+	}
   
     if (entities.typeType.length > 0) {
     user.typeType = entities.typeType
   } else if (user.typeType) {
     entities.typeType = user.typeType
-	
-  }
- 
-  
+	}
+   
     if (entities.ouvertureType.length > 0) {
     user.ouvertureType = entities.ouvertureType
   } else if (user.ouvertureType) {
     entities.ouvertureType = user.ouvertureType
+	console.log('-->')
+console.log(user.ouvertureType)
   }
 
   

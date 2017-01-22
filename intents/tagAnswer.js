d21699ceb8d28f65afc8b4f792469c8ccdd05ef0
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2017-01-22 14:24:43 +0100

    make it better

--- intents/tagAnswer.js
+++ intents/tagAnswer.js
@@ -79,7 +79,7 @@ if (!ENTITIES.nourritureType.length && !ENTITIES.boissonType.length &&!ENTITIES.
  var goodPlaces = []
 
 var ar = [
-[ENTITIES.boissonType.length, ENTITIES.boissonType.forEach, fuzzyBoisson, _.filter(datas, place => place.boissonstag.indexOf(match.value)],
+[ENTITIES.boissonType.length, ENTITIES.boissonType.forEach, fuzzyBoisson, _.filter(datas, place => place.boissonstag.indexOf(match.value))],
 [ENTITIES.nourritureType.length, nourritureType, fuzzyNourriture, nourrituretag],
 [ENTITIES.typeType.length, typeType, fuzzyType, typetag],
 ];

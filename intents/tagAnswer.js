cf5292535851b0d9d93a300dd4e9c5e7f87062b6
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2017-01-22 14:39:43 +0100

    make it better

--- intents/tagAnswer.js
+++ intents/tagAnswer.js
@@ -91,7 +91,7 @@ for (var i=0, len=ar.length; i<len; i++) {
      const match = ar[i][2].get(tag.raw);
 	 console.log('manger et...')
      if (match.distance > 0.8) {
-       goodPlaces =  ar[i][3].indexOf(match.value) !== -1)
+       goodPlaces = _.filter(goodPlaces, place => place. ar[i][3].indexOf(match.value) !== -1)
      }
  })
  

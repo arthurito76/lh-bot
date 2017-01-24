c11b512772d48b25789c5c09f6b510247000e572
Arthur FORTIN <Arthur@uc-pc-tech-tp.universal.local>
2017-01-24 11:39:16 +0100

    make it better

--- intents/tagAnswer.js
+++ intents/tagAnswer.js
@@ -108,8 +108,9 @@ if (ENTITIES.boissonType.length && ENTITIES.nourritureType.length) {
 	  
 } else if (ENTITIES.boissonType.length && !ENTITIES.nourritureType.length) {
 var tab = ENTITIES.boissonType
+console.log(tab[0])
 for (var i = 0, len = tab.length; i < len; i++) {  
-for (tab[0]) {
+if (i==0) {
  ENTITIES.boissonType.forEach(tag => {
      const match = fuzzyBoisson.get(tag.raw);
      if (match.distance > 0.8) {

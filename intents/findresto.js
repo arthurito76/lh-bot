9281815ae4234c90cc95a6f2bfe67a5a50637473
Arthur FORTIN <Arthur@uc-pc-tech-tp.universal.local>
2017-01-25 13:20:13 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -2,7 +2,6 @@ const _ = require('lodash')
 const Fuzzy = require('fuzzy-matching') 
 const utils = require('./util.js')
 const datas = require('./data.js') 
-const getEntities = require('../getEntities.js')
 const resto = datas.reduce((prev, current) => {
 if (current.name) {
 return [...prev, ...current.name];
@@ -10,17 +9,17 @@ return [...prev, ...current.name];
 }, [])
 const fuzzyResto = new Fuzzy(resto);
 
-const tagAnswer = (ENTITIES, USER) => {
+const findresto = (ENTITIES, USER) => {
 console.log('1') 
- if (!ENTITIES.restaurantName) { return Promise.resolve([utils.toText('De quel lieux parles-tu ?')])}
+ if (!RESTOINFO) { return Promise.resolve([utils.toText('De quel lieux parles-tu ?')])}
  
- ENTITIES.restaurantName.forEach(tag => {
+ ENTITIES.nourritureType.forEach(tag => {
    const match = fuzzyResto.get(tag.raw);
    console.log ('2')
        if (match.distance > 0.8) {
         const object = _.filter(datas, place => place.name.indexOf(match.value) !== -1)
 		}
-		})
+		}
 		console.log ('3')
 if (!object) { return Promise.resolve([utils.toText('Je ne trouve pas ce resto mais je vais me renseigner')])}
 const answer = []
@@ -28,9 +27,6 @@ const answer = []
   answer.push(utils.toText('sa page facebook: ' + object.page))
   answer.push(utils.toText('avis : ' + object.avis))
    return Promise.resolve(answer)
-
-const goodPlaces = _.filter(datas, place => place.tags.indexOf(RESTOINFO.raw) !== -1)
-return Promise.resolve(goodPlaces)
    
 } 
 module.exports = findresto

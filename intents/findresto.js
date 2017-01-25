7f12b656c773fb6d73c412eb4757cd4dbe973f08
Arthur FORTIN <Arthur@uc-pc-tech-tp.universal.local>
2017-01-25 12:55:14 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -2,11 +2,19 @@ const _ = require('lodash')
 const fuzzy = require('clj-fuzzy') 
 const utils = require('./util.js')
 const datas = require('./data.js') 
+const resto = datas.reduce((prev, current) => {
+if (current.name) {
+return [...prev, ...current.name];
+} else { return prev }
+}, [])
+const fuzzyResto = new Fuzzy(resto);
 const findresto = (RESTOINFO) => { 
 console.log(datas) 
  if (!RESTOINFO) { return Promise.resolve([utils.toText('De quel lieux parles-tu ?')])}
-const object =_.find(datas, data => fuzzy.metrics.jaro_winkler(data.name, RESTOINFO.raw) > 0.8)
-console.log(object)
+   const match = fuzzyResto.get(tag.raw);
+       if (match.distance > 0.8) {
+        const object = _.filter(datas, place => place.name.indexOf(match.value) !== -1)
+       }
 if (!object) { return Promise.resolve([utils.toText('Je ne trouve pas ce resto mais je vais me renseigner')])}
 const answer = []
   answer.push(utils.toText('localisation ' + object.location))

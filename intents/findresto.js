8dede289271329fe7e1ec0cf59a3f6e7e533459f
Arthur FORTIN <Arthur@uc-pc-tech-tp.universal.local>
2017-01-25 13:03:19 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -10,14 +10,14 @@ return [...prev, ...current.name];
 const fuzzyResto = new Fuzzy(resto);
 
 const findresto = (RESTOINFO) => { 
-console.log(datas) 
+console.log('1') 
  if (!RESTOINFO) { return Promise.resolve([utils.toText('De quel lieux parles-tu ?')])}
    const match = fuzzyResto.get(tag.raw);
-   console.log (match)
+   console.log ('2')
        if (match.distance > 0.8) {
         const object = _.filter(datas, place => place.name.indexOf(match.value) !== -1)
 		}
-		console.log (object)
+		console.log ('3')
 if (!object) { return Promise.resolve([utils.toText('Je ne trouve pas ce resto mais je vais me renseigner')])}
 const answer = []
   answer.push(utils.toText('localisation ' + object.location))

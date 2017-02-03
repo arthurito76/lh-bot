1ddb23dc77637d86eaf6675bd1c73c9a2ab68cd7
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2017-02-03 21:11:53 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -3,10 +3,9 @@ const fuzzy = require('clj-fuzzy')
 const utils = require('./util.js')
 const datas = require('./data.js') 
 const findresto = (RESTOINFO) => { 
-console.log(datas) 
+console.log(RESTOINFO) 
  if (!RESTOINFO) { return Promise.resolve([utils.toText('De quel lieux parles-tu ?')])}
 const object =_.find(datas, data => fuzzy.metrics.jaro_winkler(data.name, RESTOINFO.raw) > 0.8)
-console.log(object)
 if (!object) { return Promise.resolve([utils.toText('Je ne trouve pas ce resto mais je vais me renseigner')])}
 const answer = []
   answer.push(utils.toText('localisation ' + object.location))

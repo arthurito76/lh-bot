3e5bf8990df63984d4efe3fd3ca993fc7af46433
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2017-02-03 21:08:28 +0100

    make it better

--- intents/findresto.js
+++ intents/findresto.js
@@ -2,13 +2,10 @@ const _ = require('lodash')
 const fuzzy = require('clj-fuzzy') 
 const utils = require('./util.js')
 const datas = require('./data.js') 
-const getEntities = require('../getEntities.js')
-console.log(getEntities)
-const findresto = (ENTITIES, USER) => { 
- console.log(ENTITIES.restaurantName)
- if (!ENTITIES.restaurantName) { return Promise.resolve([utils.toText('De quel lieux parles-tu ?')])}
-  console.log('yo')
-const object =_.find(datas, data => fuzzy.metrics.jaro_winkler(data.name, ENTITIES.restaurantName.raw) > 0.7)
+const findresto = (RESTOINFO) => { 
+console.log(datas) 
+ if (!RESTOINFO) { return Promise.resolve([utils.toText('De quel lieux parles-tu ?')])}
+const object =_.find(datas, data => fuzzy.metrics.jaro_winkler(data.name, RESTOINFO.raw) > 0.8)
 console.log(object)
 if (!object) { return Promise.resolve([utils.toText('Je ne trouve pas ce resto mais je vais me renseigner')])}
 const answer = []
@@ -17,7 +14,7 @@ const answer = []
   answer.push(utils.toText('avis : ' + object.avis))
    return Promise.resolve(answer)
 
-const goodPlaces = _.filter(datas, place => place.tags.indexOf(ENTITIES.restaurantName.raw) !== -1)
+const goodPlaces = _.filter(datas, place => place.tags.indexOf(RESTOINFO.raw) !== -1)
 return Promise.resolve(goodPlaces)
    
 } 

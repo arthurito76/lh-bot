ddfe05bf6ede9ac0b0dd921b3876b0407be90e3d
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2017-02-04 08:02:18 +0100

    make it better

--- intents/avis.js
+++ intents/avis.js
@@ -5,22 +5,23 @@ const getEntities = require('../getEntities.js')
 const Fuzzy = require('fuzzy-matching')
 const name = datas.reduce((prev, current) => {
 if (current.name) {
+console.log(current.name)	
 return [...prev, ...current.name];
 } else { return prev }
 }, [])
 
 const fuzzyName = new Fuzzy(name);
 
-console.log(fuzzyName)
-
  const avis = (ENTITIES, USER) => {
-	 console.log(ENTITIES.restaurantName)
   
   if (!ENTITIES.restaurantName) { return Promise.resolve([utils.toText('xxxxxxxxx')])}
 
+var RESTOINFO = ENTITIES.restaurantName
+console.log(RESTOINFO)
+
   goodPlaces = []
   
-  const match = fuzzyName.get(ENTITIES.restaurantName.raw);
+  const match = fuzzyName.get(RESTOINFO.raw);
   console.log(match)
   if (match.distance > 0.5) {
     goodPlaces = _.filter(datas, place => place.name === match.value)

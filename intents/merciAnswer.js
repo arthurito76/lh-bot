5bf28779600056b5f72e017b2e3840e9114bd175
Arthur FORTIN <Arthur@uc-pc-tech-tp.universal.local>
2017-04-14 09:05:03 +0200

    make it better

--- intents/merciAnswer.js
+++ intents/merciAnswer.js
@@ -5,10 +5,11 @@ const merciAnswer = () => {
 answer.push(utils.toText('Ca me fait plaisir ;-)'))
  answer.push(utils.toText('J\'aime faire connaître les bons plan du LH'))	
   answer.push(utils.toText('De rien mon bézot'))	
-  return Promise.resolve([random(answer)])
-  USER.detailType = null
+  
+    USER.detailType = null
 console.log('coucou achat')
 USER.locationType = null
 console.log('coucou')
+  return Promise.resolve([random(answer)])
 }
 module.exports = merciAnswer

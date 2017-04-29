e5bc4326d7dbd22c02541a9a2f453405eec481c2
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2017-04-29 10:54:51 +0200

    make it better

--- intents/anniversaireAnswer.js
+++ intents/anniversaireAnswer.js
@@ -1,6 +1,6 @@
 const utils = require('./util.js')
 const random = array => { return array[Math.floor(Math.random() * array.length)] }
-const cavaAnswer = () => {
+const anniversaireAnswer = () => {
   const answer = []
 answer.push(utils.toText('Ca va être la fête à LH !'))
  answer.push(utils.toText('Pour le programme renseigne-toi auprès de la mairie, je ne peux pas tout connaître'))	
@@ -8,4 +8,4 @@ answer.push(utils.toText('Ca va être la fête à LH !'))
   return Promise.resolve([random(answer)])
   
 }
-module.exports = cavaAnswer
+module.exports = anniversaireAnswer

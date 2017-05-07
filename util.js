fb19064de8a83134d90ad1a6044a4a3f6dfc57a0
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2017-05-07 14:02:58 +0200

    make it better

--- util.js
+++ util.js
@@ -1,5 +1,6 @@
 const utils = {
   toImage: imageUrl => ({ type: 'image', content: imageUrl } ),
-  toText: message => ({ type: 'text', content: message } )
+  toText: message => ({ type: 'text', content: message } ),
+  toButtons: (title, buttons) => ({ type: 'buttons', content: buttons, title })
 }
 module.exports = utils

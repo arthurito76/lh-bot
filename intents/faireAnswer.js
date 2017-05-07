c226147fd89069b928d9b370f7bbac6525f3f992
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2017-05-07 13:42:46 +0200

    make it better

--- intents/faireAnswer.js
+++ intents/faireAnswer.js
@@ -2,9 +2,12 @@ const utils = require('./util.js')
 const faireAnswer = () => {
   const answer = []
   answer.push(utils.toText('Vaste question. Je ne suis qu\'un modeste assistant. Je me limite à répondre à des questions comme "où manger des frites", "où acheter des baskets nike" ou encore où "écouter un concert de rock". Alors en quoi puis-je t\'aider ?'))
-  utils.toButton('je veux manger', `manger`),
-    utils.toButton('je veux boire', `boire`),
-	utils.toButton('je veux acheter quelque chose', `acheter`),
+  const buttons = [
+  utils.toButton('je veux manger', 'manger', 'imBack'),
+    utils.toButton('je veux boire', 'boire', 'imBack'),
+	utils.toButton('je veux acheter quelque chose', 'acheter', 'imBack'),
+	]
+   answer.push((buttons))
   return Promise.resolve(answer)
   
 }

47ee656ffac9fd5737e95666707d757a433885ee
Arthur Fortin <arthurfortin@MacBook-Pro-de-Arthur.local>
2017-05-07 13:19:38 +0200

    make it better

--- intents/faireAnswer.js
+++ intents/faireAnswer.js
@@ -4,7 +4,6 @@ const faireAnswer = () => {
   answer.push(utils.toText('Vaste question. Je ne suis qu\'un modeste assistant. Je me limite à répondre à des questions comme "où manger des frites", "où acheter des baskets nike" ou encore où "écouter un concert de rock". Alors en quoi puis-je t\'aider ?'))
    toButton('stats', `show me ${json.name} stats`),
      toButton('moves', `show me ${json.name} moves`),
-   ]
    answer.push(toButtons('Click on them!', prompt))
   return Promise.resolve(answer)
   

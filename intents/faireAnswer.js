142a1015a83e50334009802f1e65946f962d51e5
Arthur FORTIN <Arthur@uc-pc-tech-tp.universal.local>
2017-05-09 08:56:57 +0200

    make it better

--- intents/faireAnswer.js
+++ intents/faireAnswer.js
@@ -1,4 +1,5 @@
 const utils = require('./util.js')
+console.log(utils)
 const faireAnswer = () => {
   const answer = []
   answer.push(utils.toText('Vaste question. Je ne suis qu\'un modeste assistant. Je me limite à répondre à des questions comme "où manger des frites", "où acheter des baskets nike" ou encore où "écouter un concert de rock". Alors en quoi puis-je t\'aider ?'))

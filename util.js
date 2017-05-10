31127db0a6bbbc9b894c069ee3cb82b3f9ff313b
arthurito76 <Arthur@uc-pc-tech-tp.universal.local>
2017-05-10 15:57:21 +0200

    make it better

--- util.js
+++
@@ -1,7 +0,0 @@
-const utils = {
-  toImage: imageUrl => ({ type: 'image', content: imageUrl } ),
-  toText: message => ({ type: 'text', content: message } ),
-  toButtonsTest: (title, buttons) => ({ type: 'buttons', content: buttons, title }),
-toButton: (title, value) => ( { title, value } )
-}
-module.exports = utils

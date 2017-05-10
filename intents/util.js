ccdca6f3dd10a966116e01238084e640f005e904
arthurito76 <Arthur@uc-pc-tech-tp.universal.local>
2017-05-10 10:46:11 +0200

    make it better

--- intents/util.js
+++ intents/util.js
@@ -1,7 +1,7 @@
- const utils = {
- toImage: imageUrl => ({ type: 'image', content: imageUrl } ),
- toText: message => ({ type: 'text', content: message } ),
- toButton: (title, value, type) => { return { title, value, type } },
- toCarousel: (cards) => { return { type: 'carousel', cards } },
+const utils = {
+  toImage: imageUrl => ({ type: 'image', content: imageUrl } ),
+  toText: message => ({ type: 'text', content: message } ),
+  toButtonsTest: (title, buttons) => ({ type: 'buttons', content: buttons, title }),
+toButton: (title, value) => ( { title, value } )
 }
-module.exports = utils 
+module.exports = utils

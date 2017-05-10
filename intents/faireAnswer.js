88b16ce19cfe0967d86930606e66f00c63222148
arthurito76 <Arthur@uc-pc-tech-tp.universal.local>
2017-05-10 12:29:58 +0200

    make it better

--- intents/faireAnswer.js
+++ intents/faireAnswer.js
@@ -4,10 +4,11 @@ const faireAnswer = () => {
   const answer = []
   answer.push(utils.toText('Vaste question. Je ne suis qu\'un modeste assistant. Je me limite à répondre à des questions comme "où manger des frites", "où acheter des baskets nike" ou encore où "écouter un concert de rock". Alors en quoi puis-je t\'aider ?'))
   const boutton = [
-     utils.toButton('stats', `les stats`),
-     utils.toButton('moves', `show me`),
+     utils.toButton('je veux manger', `je veux manger`),
+     utils.toButton('je veux boire', `je veux boire`),
+	 utils.toButton('je veux acheter quelque chose', `je veux acheter`),
    ]
-   answer.push(utils.toButtonsTest('yo', boutton))
+   answer.push(utils.toButtonsTest('Que veux-tu faire ?', boutton))
   return Promise.resolve(answer)
   
 }

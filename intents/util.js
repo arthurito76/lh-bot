363e193e38966830d904fd9317e486e14807cf37
arthurito76 <Arthur@uc-pc-tech-tp.universal.local>
2017-05-11 12:54:07 +0200

    make it better

--- intents/util.js
+++ intents/util.js
@@ -3,6 +3,6 @@
  toText: message => ({ type: 'text', content: message } ),
  toButton: (title, value, type) => { return { title, value, type } },
  toCarousel: (cards) => { return { type: 'carousel', cards } },
- toButtonsTest: (title, buttons) => ({ type: 'buttons', content: buttons, title }),
+  toButtonsTest: (title, buttons) => ({ type: 'quickReplies', content: {title: 'TITLE', buttons: [{title: 'BUTTON_TITLE', value: 'BUTTON_VALUE',},]}}),
 }
 module.exports = utils 
